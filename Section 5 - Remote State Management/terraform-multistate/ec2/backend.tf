dcfe799b84059099004617a092b68a7d0f5e1daf
ragook6 <ragook2@gmail.com>
2024-05-22 16:53:47 -0400

    added terraform files

--- Section 5 - Remote State Management/terraform-multistate/ec2/backend.tf
+++ Section 5 - Remote State Management/terraform-multistate/ec2/backend.tf
@@ -4,6 +4,8 @@ terraform {
     bucket = "kplabs-remote-backend"
     key    = "ec2demo.tfstate"
     region = "us-west-1"
+    access_key = "AKIAJXBRJCPEBT4AUO6A"
+    secret_key = "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"
     dynamodb_table = "s3-state-lock"
   }
 }
